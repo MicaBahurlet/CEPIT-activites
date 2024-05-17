6ff5a50a6b5b5b1013be044b8cde0a6f974d692d
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-05-17 18:39:37 -0300

    Add exercise 2

---
+++ Entregas/TrabajoN2/Ejercicio2.js
@@ -0,0 +1,24 @@
+//Array de números
+var numeros = [4, 7, 9, 3, 1, 45, 67, 23, 29, 78, 11, 16];
+// Para poder encontrar el number más grande 
+var numeroMasGrande = numeros[0]; // Que es el primero
+// bucle for para recorrr if encuentra un número más grande que numeroMasGrande al recorrer el length del array lo actualiza
+for (var i = 1; i < numeros.length; i++) {
+    if (numeros[i] > numeroMasGrande) {
+        numeroMasGrande = numeros[i];
+    }
+}
+// por consola
+console.log("El número más grande del array es:", numeroMasGrande);
+// para determinar si el número es par o impar
+function esParOImpar(numero) {
+    if (numero % 2 === 0) {
+        return "par";
+    }
+    else {
+        return "impar";
+    }
+}
+// Imprimir en consola
+var resultado = esParOImpar(numeroMasGrande);
+console.log("El número más grande es", numeroMasGrande, "y es", resultado);
--- readme.md
+++ readme.md
@@ -7,3 +7,9 @@ Carpeta de entregas
 <b>TrabajoN2:</b> Dado el diguiente arreglo [4,7,9,3,1,45,67,23,29,78,11,16]
 - Crear un programa que encuentre cual es el número más grandee del arreglo e imprimirlo por consola
 - Almacenar el numero más grande en una viariable global y pasarlo a una funcion  para determinar si el número es par o impar 
+
+<b>TrabajoN3:</b> Realice un programa que devuelva la potencia de un número
+La base y el exponente deben ser ingresados por teclado
+Solo deben admitirse exponentes mayores o iguales a cero
+Recuerde que el resultado de un numeroe levado a 0 es 1
+Separe la logica de calcular la potencia utilizando metodos
